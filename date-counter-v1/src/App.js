e38d450bec294d15821e01f2b700c71331cfd028
Sanjay Dorairaj <dorairajsanjay@yahoo.com>
2023-11-13 06:23:15 -0800

    adding files for start projects

---
+++ date-counter-v1/src/App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  return (
+    <div>
+      <p>starter code</p>
+    </div>
+  );
+}
